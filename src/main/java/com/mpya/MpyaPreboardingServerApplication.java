cdb8624d3e54cdf6a207a516f87de1f33c0cb305
imadcollin <imad.collin@gmail.com>
2020-09-25 10:47:30 +0200

    Initial commit

---
+++ src/main/java/com/mpya/MpyaPreboardingServerApplication.java
@@ -0,0 +1,15 @@
+package com.mpya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpyaPreboardingServerApplication {
+
+    public static void main(String[] args) {
+
+        System.out.print("test everything is OK!");
+        SpringApplication.run(MpyaPreboardingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpya/MpyaPreboardingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpyaPreboardingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
