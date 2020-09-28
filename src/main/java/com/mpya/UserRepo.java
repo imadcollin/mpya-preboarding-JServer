cb13393bbd929c5da653d16b577e8838a82d23c8
imadcollin <imad.collin@gmail.com>
2020-09-28 08:28:13 +0200

    Replace Jpa with mongodb repository

--- src/main/java/com/mpya/UserRepo.java
+++ src/main/java/com/mpya/UserRepo.java
@@ -1,7 +1,11 @@
 package com.mpya;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-interface  UserRepo extends JpaRepository<User, Long> {
+import java.util.List;
+
+public interface UserRepo extends MongoRepository<User, Object> {
+    List<User> findByFirstName(final String country);
 
 }
